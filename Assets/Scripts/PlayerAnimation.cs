75acc1ca31b1e501ec2eaef1beecc24a6cbd7083
Julaxe <julaxe8@gmail.com>
2022-04-20 11:53:54 -0400

    Player movement and animation done

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+    
+    private readonly int _hashRForwardAnimation = Animator.StringToHash("RollingForward");
+    private readonly int _hashRBackwardsAnimation = Animator.StringToHash("RollingBackwards");
+    private bool _isRolling = false;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isRolling = !_isRolling;
+            _animator.SetBool(_hashRForwardAnimation, _isRolling);
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rigidbody;
+
+    [SerializeField] private float _speed = 1f;
+
+    private Vector3 _direction;
+    private Vector3 _force;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        _direction = new Vector3(x, 0f, y);
+        if (_direction != Vector3.zero)
+        {
+            _direction = _direction.normalized;
+            var newRot = Quaternion.LookRotation(_direction);
+            transform.rotation = newRot;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        _force = _direction * _speed * Time.fixedDeltaTime;
+        _rigidbody.AddForce(_force);
+    }
+
 }
